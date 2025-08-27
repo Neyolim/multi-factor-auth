8aa62dc85b5e09631079dade7f849ec569a704ed
Neyolim <neyoollim@gmail.com>
2025-08-27 12:50:33 +0545

    basic setup

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import express, { urlencoded } from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+import dbConnect from "./config/dbConnect.js";
+
+dotenv.config();
+
+dbConnect();
+const app = express();
+
+
+// Middlewares
+const corsOptions = {
+  origin: ["http://localhost:3001"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+
+// Listen app
+const PORT = process.env.PORT || 7002;
+app.listen(PORT, () => {
+  console.log(`App is listening at port ${PORT}`);
+});
