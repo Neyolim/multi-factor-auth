a020009fbb1954775a77fb72cc8020e08a94f1b2
Neyolim <neyoollim@gmail.com>
2025-08-28 22:43:18 +0545

    add two-factor authentication (setup, verify, reset) with testing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,17 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.js";
+import speakeasy from "speakeasy";
+import qrCode from "qrcode";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return res.status(400).json({ error: "Username and password are required" });
+      return res
+        .status(400)
+        .json({ error: "Username and password are required" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,47 +24,103 @@ export const register = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ 
-      message: "User registered successfully", 
-      user: { id: newUser._id, username: newUser.username }
+    res.status(201).json({
+      message: "User registered successfully",
+      user: { id: newUser._id, username: newUser.username },
     });
-
   } catch (error) {
     if (error.code === 11000) {
       return res.status(400).json({ error: "Username already exists" });
     }
-    res.status(500).json({ error: "Error registering user", message: error.message });
+    res
+      .status(500)
+      .json({ error: "Error registering user", message: error.message });
   }
 };
 
-
 export const login = async (req, res) => {
-   console.log("The authenticate user is : ", req.user);
-   res.status(200).json({
+  console.log("The authenticate user is : ", req.user);
+  res.status(200).json({
     message: "User logged in successfully",
     isMfaActive: req.user.isMfaActive,
-   });
+  });
 };
 
-
 export const authStatus = async (req, res) => {
-  if(req.user){
+  if (req.user) {
     res.status(200).json({
       message: "User logged in successfully",
       username: req.user.username,
       isMfaActive: req.user.isMfaActive,
     });
-  }else{
-    res.status(401).json({ message: "Unauthorized user"});
+  } else {
+    res.status(401).json({ message: "Unauthorized user" });
   }
 };
 export const logout = async (req, res) => {
-  if(!req.user) res.status(401).json({ message: "Unauthorized user"});
+  if (!req.user) res.status(401).json({ message: "Unauthorized user" });
   req.logout((err) => {
-    if (err) res.status(400).json({ message: "User not logged in"});
-    res.status(200).json({message: "Logout is successful"});
+    if (err) res.status(400).json({ message: "User not logged in" });
+    res.status(200).json({ message: "Logout is successful" });
   });
 };
-export const setup2FA = async (req, res) => {};
-export const verify2FA = async (req, res) => {};
-export const reset2FA = async (req, res) => {};
+export const setup2FA = async (req, res) => {
+  try {
+    console.log("The req.user is: ", req.user);
+    const user = req.user;
+    var secret = speakeasy.generateSecret();
+    console.log("The secret object is : ", secret);
+    user.twoFactorSecret = secret.base32;
+    user.isMfaActive = true;
+    await user.save();
+    const url = speakeasy.otpauthURL({
+      secret: secret.base32,
+      label: `${req.user.username}`,
+      issuer: "www.neyollim.com",
+      encoding: "base32",
+    });
+    const qrImageUrl = await qrCode.toDataURL(url);
+    res.status(200).json({
+      secret: secret.base32,
+      qrCode: qrImageUrl,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error setting up 2FA", message: error.message });
+  }
+};
+export const verify2FA = async (req, res) => {
+  const { token } = req.body;
+  const user = req.user;
+
+  const verified = speakeasy.totp.verify({
+    secret: user.twoFactorSecret,
+    encoding: "base32",
+    token,
+  });
+
+  if (verified) {
+    const jwtToken = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1hr",
+      }
+    );
+    res.status(200).json({ message: "2FA Successful", token: jwtToken });
+  } else {
+    res.status(400).json({ message: "Invalid 2FA Token" });
+  }
+};
+export const reset2FA = async (req, res) => {
+  try {
+    const user = req.user;
+    user.twoFactorSecret = "";
+    user.isMfaActive = false;
+    await user.save();
+    res.status(200).json({ message: "2FA reset successful"});
+  } catch (error) {
+    res.status(500).json({ message:"Error reseting 2FA", error: error})
+  }
+};
